81d3e9b562931244c91ba7d643ac2d903d89129e
Daniel Beal <git@dbeal.dev>
2023-11-13 21:05:30 +0900

    ability to supply settings

--- target_chains/ethereum/contracts/contracts/pyth/PythMock.sol
+++ target_chains/ethereum/contracts/contracts/pyth/PythMock.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 import "@pythnetwork/pyth-sdk-solidity/MockPyth.sol";
 
 contract PythMock is MockPyth {
-	constructor() MockPyth(300, 0) {}
+	constructor(uint _validTimePeriod, uint _singleUpdateFeeInWei) MockPyth(_validTimePeriod, _singleUpdateFeeInWei) {}
 }
